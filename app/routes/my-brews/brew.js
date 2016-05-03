1e3935518d5b005426086d077798dc34546a01eb
Ben Harrison <ben@benthedeveloper.com>
2016-05-02 17:05:37 -0700

    Updated app name. Need to get tabs working.

--- app/routes/my-brews/brew.js
+++ app/routes/my-brews/brew.js
@@ -1,7 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
 	model(params) {
 		return this.store.findRecord('brew', params.brew_id);
+	},
+
+	actions: {
+		save(curBrew) {
+			alert('implement save action');
+		},
+
+		cancel() {
+			this.transitionTo('my-brews.in-progress');
+		},
+
+		willTransition() {
+			this.controller.get('model').rollbackAttributes();
+		}
 	}
+
 });
--- app/routes/my-brews/completed.js
+++ app/routes/my-brews/completed.js
@@ -1,8 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model() {
-		var brews = this.store.findAll('brew'); // => GET /brews
-		//return brews;
-	}
 });
